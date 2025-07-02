d70e608a7634286a6447092d9f49dd12a71edfd9
vinhkhanh <khanhnguyeen2208@gmail.com>
2025-07-02 08:22:04 +0700

    Commit01_VinhKhanh: done Dashboard, WelcomJdialog, Package dao

---
+++ src/main/java/dao/BedsitDAO.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package dao;
+
+import entity.Bedsit;
+
+/**
+ *
+ * @author nhukhue
+ */
+public interface BedsitDAO extends CrudDAO<Bedsit, String>{
+    
+}
