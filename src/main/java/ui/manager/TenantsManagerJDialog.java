91883fc17d431bd278b93a2a1b4d5954af214176
trinh1804 <phamthuytrinh1804@gmail.com>
2025-07-18 08:17:53 +0700

    update

--- src/main/java/ui/manager/TenantsManagerJDialog.java
+++ src/main/java/ui/manager/TenantsManagerJDialog.java
@@ -319,8 +319,8 @@ public class TenantsManagerJDialog extends javax.swing.JDialog implements Tenant
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(btnMoveLast, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addContainerGap())
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGap(30, 30, 30)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jLabel9)
                     .addGroup(jPanel2Layout.createSequentialGroup()
@@ -356,7 +356,7 @@ public class TenantsManagerJDialog extends javax.swing.JDialog implements Tenant
                                 .addComponent(txtDateOfBirth)
                                 .addComponent(txtEmail)
                                 .addComponent(txtFullName, javax.swing.GroupLayout.PREFERRED_SIZE, 230, javax.swing.GroupLayout.PREFERRED_SIZE)))))
-                .addGap(29, 29, 29))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
